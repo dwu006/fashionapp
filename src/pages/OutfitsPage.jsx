88b36d6cd93b4a6e32272ab751862e2d32af25b9
JMDETETH <kevintr1005@gmail.com>
2025-03-02 19:40:00 -0800

    Fixed comment section
    Modified how the likes and comments section work and how the post outfits thing look like for easy access.

--- src/pages/OutfitsPage.jsx
+++ src/pages/OutfitsPage.jsx
@@ -1,15 +1,38 @@
 import UserHeader from "../components/UserHeader.jsx";
 import Footer from "../components/Footer.jsx";
 import GenerateOutfit from "../components/GenerateOutfit.jsx";
+import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import "../styles/PageLayout.css";
 
 function OutfitsPage() {
+    const [userId, setUserId] = useState('')
     const isAuthenticated = localStorage.getItem('token');
 
     if (!isAuthenticated) {
         return <Navigate to="/login" />;
     }
+
+    useEffect(() => {
+        // Fetch user profile to get user ID when component mounts
+        const fetchUserId = async () => {
+            try {
+                const response = await fetch('http://localhost:5001/users/profile', {
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setUserId(data._id);
+                }
+            } catch (error) {
+                console.error('Error fetching user ID:', error);
+            }
+        };
+        fetchUserId();
+    }, [userId]);
 
     return (
         <div className="page feed">
@@ -19,7 +42,7 @@ function OutfitsPage() {
                     <h3>Generate Outfit</h3>
                     <button className="button">Saved Outfits</button>
                 </div>
-                <GenerateOutfit />
+                <GenerateOutfit userId={userId}/>
             </div>
             <Footer />
         </div>
